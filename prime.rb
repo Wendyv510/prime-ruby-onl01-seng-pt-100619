f2223abeae1e1f2dd9a8c5f1dec6465c8b210a2a
Wendy Harris <wendyv510@hotmail.com>
2019-10-19 16:40:17 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,13 @@
-require './prime_spec.rb'
+
 
 
 def prime?(integer)
-  integer = (-10,000 .. 10,000))to_a 
+  integer = (-10,000 .. 10,000).to_a 
   prime?.each do |number|
-    if {|number| n / n == n}
-      "prime number"
-    else {|number| n / n 
-      "composite number"
+    if {|number| n / n == 1}
+      "true, prime number"
+    else {|number| n / 3 == (integer), n/2 == (integer), n/5 == (integer)} 
+      "false, composite number"
     end 
   end 
   
